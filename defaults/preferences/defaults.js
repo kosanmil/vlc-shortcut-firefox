40e3ace4d7118624f3eb59329fbe174c4cd8dda5
dyrnade <gurescicem@gmail.com>
2016-11-14 02:21:00 +0200

    added video quality preference

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,10 +1,11 @@
-//var osString = Components.classes["@mozilla.org/xre/app-info;1"]  
+//var osString = Components.classes["@mozilla.org/xre/app-info;1"]
 //                   .getService(Components.interfaces.nsIXULRuntime).OS;
 //if(true)
 pref("extensions.vlc_shortcut.vlc_filepath", "");
 pref("extensions.vlc_shortcut.middleclick", 0);
 pref("extensions.vlc_shortcut.ctrlmiddleclick", 0);
 pref("extensions.vlc_shortcut.shiftmiddleclick", 0);
+pref("extensions.vlc_shortcut.vq", -1);
 //else
 //	pref("extensions.vlc_shortcut.vlc_filepath", "BLABLABLA");
 //Components.classes["@mozilla.org/preferences-service;1"]
